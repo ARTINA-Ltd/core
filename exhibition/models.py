c4f3ec9a14ce0d50ab121bfed7c45802b08f7e90
shervindadashzade <shervin.dadashzade7988@gmail.com>
2022-08-13 14:39:03 +0430

    Auction and offer models added

--- exhibition/models.py
+++ exhibition/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from core.models import NFT
+
+
+from datetime import datetime
 # Create your models here.
 
 class Exhibition():
@@ -15,3 +18,30 @@ class NFtEx():
     nft = models.ForeignKey(NFT)
     ex=models.ForeignKey(Exhibition)
     date = models.DateTimeField(verbose_name="تاریخ", auto_now=True)
+
+class Auction(models.Model):
+    nft_ex = models.ForeignKey(NFtEx)
+    floor_price = models.CharField(max_length=15,null=False, blank=False)
+    startdate = models.DateField(verbose_name='تاریخ')
+    enddate = models.DateField(verbose_name='تاریخ')
+
+    def has_expired(self):
+        return datetime.now() > self.enddate
+    
+    def get_winner(self):
+        if self.has_expired():
+            offers = self.offer_set.all()
+            winner = max(offers, key = (lambda x: x.price))
+            return winner.user
+        else:
+            return {'error':'The auction has not expired yet.'}
+
+
+
+class Offer(models.Model):
+    user = models.ForeignKey(User)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    price = models.CharField(max_length=15, null=False, blank=False)
+    status = models.CharField(max_length=5, choices=[('O','open'),('C','close')])
+
+    # Make an offer would be handdled on serializer.
