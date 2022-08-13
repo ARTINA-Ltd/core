d6dd1557a3eed7d6be5fb3ff573c7065818c1713
jestemAria <radmehr.krash@gmail.com>
2022-08-13 13:05:08 +0430

    adding or deleting spaces between code for better readability

--- exhibition/models.py
+++ exhibition/models.py
@@ -5,14 +5,14 @@ from core.models import NFT
 
 
 class Exhibition(models.Model):
-    user=models.ForeignKey(User,on_delete = models.CASCADE)
-    marketName=models.CharField(max_length=15,null=False,blank=False)
-    image=models.ImageField( upload_to = "Exhibition" , verbose_name="Exhibition",null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    marketName = models.CharField(max_length=15, null=False, blank=False)
+    image = models.ImageField(upload_to="Exhibition", verbose_name="Exhibition", null=True, blank=True)
     startdate = models.DateTimeField(verbose_name="تاریخ")
     enddate = models.DateTimeField(verbose_name="تاریخ")
 
 
 class NFtEx(models.Model):
     nft = models.ForeignKey(NFT, on_delete=models.CASCADE)
-    ex=models.ForeignKey(Exhibition, on_delete=models.CASCADE)
+    ex = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
     date = models.DateTimeField(verbose_name="تاریخ", auto_now=True)
