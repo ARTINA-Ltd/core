67ff11d4ed0cc25a7104cd7b86a777f3a106fe77
jestemAria <radmehr.krash@gmail.com>
2022-08-13 13:27:03 +0430

    adding `_` between variable names for better readability

--- Account/models.py
+++ Account/models.py
@@ -16,10 +16,10 @@ class Role (models.Model):
 
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    nationalcode = models.CharField(max_length=10, verbose_name="کدملی", null=True, blank=True)
+    national_code = models.CharField(max_length=10, verbose_name="کدملی", null=True, blank=True)
     birthdate = models.CharField(max_length=10, verbose_name="تاریخ تولد", null=True, blank=True)
-    phonenumber = models.CharField(max_length=11, verbose_name="شماره تلفن", null=True, blank=True)
-    cellnumber = models.CharField(max_length=11, verbose_name="ثابت شماره تلفن", null=True, blank=True)
+    phone_number = models.CharField(max_length=11, verbose_name="شماره تلفن", null=True, blank=True)
+    cell_number = models.CharField(max_length=11, verbose_name="ثابت شماره تلفن", null=True, blank=True)
     address = models.TextField(max_length=200, verbose_name="آدرس", null=True, blank=True)
     national_code_picture = models.ImageField(verbose_name="عکس کارت ملی", upload_to="pictures of users", null=True,
                                               blank=True)
