445382bf1586cb75b41de6804b41939f0ddf64cb
jestemAria <radmehr.krash@gmail.com>
2022-08-13 13:44:39 +0430

    adding parameter `models.Model` as argument of classes

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class NFT():
+class NFT(models.Model):
     name = models.CharField(max_length=15, null=False, blank=False)
     owner = models.ForeignKey(User, null=False, blank=False)
     creator = models.CharField(max_length=15, null=False, blank=False)
@@ -12,7 +12,7 @@ class NFT():
     lastPrice = models.CharField(max_length=15, null=False, blank=False)
 
 
-class Transaction():
+class Transaction(models.Model):
     nft = models.ForeignKey(NFT)
     seller = models.CharField(max_length=15, null=False, blank=False)
     buyer = models.CharField(max_length=15, null=False, blank=False)
@@ -24,7 +24,7 @@ class Wallet(models.Model):
     address = models.CharField(verbose_name="wallet address", max_length=100)
 
 
-class Order():
+class Order(models.Model):
     nft = models.ManyToManyField(NFT)
     bidder = models.ForeignKey(User, null=False, blank=False)
     fee = models.IntegerField(verbose_name="قیمت", null=False, blank=False)
