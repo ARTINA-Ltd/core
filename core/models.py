d95dcc0e5a8d99bfc32f79f4c2ce24b6aee72c98
jestemAria <radmehr.krash@gmail.com>
2022-08-13 13:47:29 +0430

    adding `on_delete` parameter on foreign keys

--- core/models.py
+++ core/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 
 class NFT(models.Model):
     name = models.CharField(max_length=15, null=False, blank=False)
-    owner = models.ForeignKey(User, null=False, blank=False)
+    owner = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     creator = models.CharField(max_length=15, null=False, blank=False)
     date = models.DateTimeField(verbose_name="تاریخ", auto_now=True)
     lastPrice = models.CharField(max_length=15, null=False, blank=False)
 
 
 class Transaction(models.Model):
-    nft = models.ForeignKey(NFT)
+    nft = models.ForeignKey(NFT, on_delete=models.CASCADE)
     seller = models.CharField(max_length=15, null=False, blank=False)
     buyer = models.CharField(max_length=15, null=False, blank=False)
     date = models.DateTimeField(verbose_name="تاریخ", auto_now=True)
@@ -26,6 +26,6 @@ class Wallet(models.Model):
 
 class Order(models.Model):
     nft = models.ManyToManyField(NFT)
-    bidder = models.ForeignKey(User, null=False, blank=False)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     fee = models.IntegerField(verbose_name="قیمت", null=False, blank=False)
     date = models.DateTimeField(verbose_name="تاریخ", auto_now=True)
