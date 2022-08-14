a38143a0fe6d3d746f8c6fe91cf18b98767a94c2
alikhazaei <alikhazaee00@gmail.com>
2022-08-14 16:07:23 +0430

    updated

--- Account/models.py
+++ Account/models.py
@@ -21,4 +21,4 @@ class Profile (models.Model):
     address= models.TextField(max_length=200,verbose_name="آدرس",null=True,blank=True)
     national_code_picture=models.ImageField(verbose_name="عکس کارت ملی",upload_to="pictures of users",null=True,blank=True)
     image=models.ImageField( upload_to = "عکس پروفایل" , verbose_name="عکس پروفایل",null=True,blank=True)
-    role=models.ForeignKey(Role)
+    role=models.ForeignKey(Role, on_delete=models.CASCADE)
