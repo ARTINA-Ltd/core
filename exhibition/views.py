dfebf83ae24529d6155d6a71b50015d320fcca48
alikhazaei <alikhazaee00@gmail.com>
2022-08-16 17:20:57 +0430

    create function createEx()

--- exhibition/views.py
+++ exhibition/views.py
@@ -1,11 +1,20 @@
 # Create your views here.
 from rest_framework import viewsets
 
-from .models import *
+from exhibition.models import *
 from .serializer import *
+import datetime
 
 
 class ExhibitionList(viewsets.ModelViewSet):
     queryset = Exhibition.objects.all()
     serializer_class = ExhibitionSerializer
 
+
+# a,b,c,d  inputs
+def createEx():
+    new = Exhibition.objects.create(user = User.objects.get(id=1),marketName='Ape',image = '' ,startdate = datetime.datetime(2022,10,10) , enddate = datetime.datetime(2022,5,5))
+    new.save()
+
+
+createEx()
