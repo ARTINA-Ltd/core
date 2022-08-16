136fd3ed17bd7392beb1d66a80a1f6765425f60b
alikhazaei <alikhazaee00@gmail.com>
2022-08-16 18:11:54 +0430

    comment kardam function farakhunde shodaro chon momken bud error begirin

--- exhibition/views.py
+++ exhibition/views.py
@@ -16,5 +16,6 @@ def createEx():
     new = Exhibition.objects.create(user = User.objects.get(id=1),marketName='Ape',image = '' ,startdate = datetime.datetime(2022,10,10) , enddate = datetime.datetime(2022,5,5))
     new.save()
 
+# run function below to create new exhibitions
 
-createEx()
+# createEx()
