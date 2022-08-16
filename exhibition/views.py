bd0990c410b996a892ddc3c0a8f7fe1e24f9f6b8
alikhazaei <alikhazaee00@gmail.com>
2022-08-16 12:55:27 +0430

    view updated

--- exhibition/views.py
+++ exhibition/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from rest_framework import viewsets
+
+from .models import *
+from .serializer import *
+
+
+class ExhibitionList(viewsets.ModelViewSet):
+    queryset = Exhibition.objects.all()
+    serializer_class = ExhibitionSerializer
+
