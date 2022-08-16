baeee96eb3a802e49d470c23dd0f4a1339885b3e
alikhazaei <alikhazaee00@gmail.com>
2022-08-16 12:55:00 +0430

    artina/url has been updated

--- ARTINA/urls.py
+++ ARTINA/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('transactions/', include('core.urls'))
+    path('transactions/', include('core.urls')),
+    path('exhibitions/', include('exhibition.urls')),
 ]
