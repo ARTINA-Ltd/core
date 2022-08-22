88c1872948d0d14d97946d6d689065cb36c0c212
shervindadashzade <shervin.dadashzade7988@gmail.com>
2022-08-22 12:33:45 +0430

    some minor fixes on models to be migratable.

--- exhibition/models.py
+++ exhibition/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from core.models import NFT
-
+from django.utils import timezone
 from datetime import datetime
 # Create your models here.
 
@@ -11,8 +11,8 @@ class Exhibition(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     marketName = models.CharField(max_length=15, null=False, blank=False)
     image = models.ImageField(upload_to="./pictures of Exhibitions", verbose_name="Exhibition", null=True, blank=True)
-    start_date = models.DateTimeField(verbose_name="تاریخ شروع")
-    end_date = models.DateTimeField(verbose_name="تاریخ پایان")
+    start_date = models.DateTimeField(verbose_name="تاریخ شروع", default=timezone.now)
+    end_date = models.DateTimeField(verbose_name="تاریخ پایان", default=timezone.now)
 
     def __str__(self):
         return f'{self.marketName} by {self.user.username}' 
