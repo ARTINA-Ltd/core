493e6f8fb55673d9f4e78762ef6642cee68edc1c
jestemAria <radmehr.krash@gmail.com>
2022-08-22 13:24:59 +0430

    serializers.py updated with start_date and end_date arguments

--- exhibition/serializers.py
+++ exhibition/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from exhibition import models
 
+
 class ExhibitionSerializer(serializers.ModelSerializer):
     
     nfts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = models.Exhibition
-        fields = ['user','marketName','image','startdate','enddate','nfts']
+        fields = ['user', 'marketName', 'image', 'start_date', 'end_date', 'nfts']
+
 
 class NFtExSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.NFtEx
-        fields = ['nft','ex']
+        fields = ['nft', 'ex']
