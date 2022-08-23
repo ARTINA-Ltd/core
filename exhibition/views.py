7ad08960d827eb9b714e65cc9a05c31b6b7b7e36
alikhazaei <alikhazaee00@gmail.com>
2022-08-23 14:36:56 +0430

    id ro migire miad field is_accepted o true mikone be onvane nft k tavasote exhibitor accept shode

--- exhibition/views.py
+++ exhibition/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
+from ast import Pass
+from cgi import print_form
 from operator import ne
+import pstats
 from rest_framework import viewsets
 from exhibition import models
 from exhibition import serializers
@@ -32,4 +35,16 @@ def register_artwork_for_exhibition():
     new = models.NFtEx.objects.create(nft = models.NFT.objects.get(id=1), ex = models.Exhibition.objects.get(id=1), date = datetime.datetime.now, is_nft_viewed_by_exhibitor = False, is_nft_accepted_by_exhibitor = False)
     new.save()
 #run func below to add a nft to an exhibition
+
 # register_artwork_for_exhibition()    
+
+
+#pass the artwork id to change is_accepted IF EXHIBITOR CONFIRM THE REQUEST
+def check_register_artwork_by_exhibitor():
+    new = models.NFtEx.objects.get(id=1)
+    new.is_nft_accepted_by_exhibitor = True
+    new.save()
+ 
+#run func below when exhibitor accept the NFT request
+
+# check_register_artwork_by_exhibitor()
