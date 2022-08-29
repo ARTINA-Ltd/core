df6d438fc9fb0c041c13a4e4d39bd947fe11ee5e
jestemAria <radmehr.krash@gmail.com>
2022-08-29 12:41:46 +0430

    optimized and cleared codes

--- Account/views.py
+++ Account/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,20 +9,19 @@ from exhibition.serializers import NFtExSerializer
 
 
 class ArtistViewSet(viewsets.ModelViewSet):
-    # TODO:: filter the artists only when roles added.
+    # TODO: filter the artists only when roles added.
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
-    def get_exhibition_from_artist(self,artist, is_accepted):
+    def get_exhibition_from_artist(self, artist, is_accepted):
         nfts = artist.nft_set.all()
         exhibitions = []
         for nft in nfts:
-            if nft.exhibition.first() != None:
+            if nft.exhibition.first() is not None:
                 exhibitions.append(nft.exhibition.first())
         exhibitions = list(set(exhibitions))
         exhibitions = list(filter(lambda x: x.is_nft_accepted_by_exhibitor == is_accepted, exhibitions))
         return exhibitions
-
 
     @action(detail=True, methods=['get'], name='Get Applications')
     def get_applications(self, request, pk=None):
@@ -33,15 +31,14 @@ class ArtistViewSet(viewsets.ModelViewSet):
             
             return Response(NFtExSerializer(exhibitions, many=True).data, status.HTTP_200_OK)
         except User.DoesNotExist:
-            return Response({'error':'User does not exist.'}, status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'User does not exist.'}, status.HTTP_400_BAD_REQUEST)
 
-    
     @action(detail=True, methods=['get'], name='Get Exhibitions')
     def get_exhibitions(self, request, pk=None):
         try:
             artist = User.objects.get(id=pk)
             exhibitions = self.get_exhibition_from_artist(artist, True)
-            exhibitions = list(map(lambda x:x.ex, exhibitions))
+            exhibitions = list(map(lambda x: x.ex, exhibitions))
             exhibitions = list(set(exhibitions))
             data = []
             for exhibition in exhibitions:
@@ -57,7 +54,7 @@ class ArtistViewSet(viewsets.ModelViewSet):
                 data.append(exhibition_serialized)
             return Response(data, status.HTTP_200_OK)
         except User.DoesNotExist:
-            return Response({'error':'User does not exist.'}, status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'User does not exist.'}, status.HTTP_400_BAD_REQUEST)
 
     
     
