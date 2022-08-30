8d578932e691869efc451005043a1d7bd49b7efa
alikhazaei <alikhazaee00@gmail.com>
2022-08-30 15:30:35 +0430

    get exhibitions by exhibitor added

--- exhibition/views.py
+++ exhibition/views.py
@@ -3,8 +3,11 @@ from core.models import NFT
 from exhibition import models
 from exhibition import serializers
 import datetime
+from rest_framework.response import Response
 from django.contrib.auth.models import User
-
+from rest_framework.decorators import action
+from django.http import JsonResponse
+import exhibition
 
 class ExhibitionViewSet(viewsets.ModelViewSet):
     queryset = models.Exhibition.objects.all()
@@ -16,51 +19,39 @@ class NFtExView(viewsets.ModelViewSet):
     serializer_class = serializers.NFtExSerializer
 
 
-# a,b,c,d  inputs
-def create_exhibition():
-    new = models.Exhibition.objects.create(user=User.objects.get(id=3), marketName='Ape', image='',
-                                           start_date=datetime.datetime(2022, 10, 10),
-                                           end_date=datetime.datetime(2022, 5, 5))
-    new.save()
 
-# run function below to create new exhibitions
+class ExhibitorViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
 
-# create_exhibition()
+    def get_serializer_class(self):
+        if self.action =='get_exhibitions':
+            return serializers.ExhibitionSerializer
+        else:
+            return super().get_serializer_class()
 
+    @action(detail=True, methods=['get'], name='Get Exhibitions')
+    def get_exhibitions(self, request, pk=None):
+        exhibitor = User.objects.get(id=pk)
+        exhibition = exhibitor.exhibition_set.all()
+        exhibitions = serializers.ExhibitionSerializer(exhibition, many = True)
+        return Response(exhibitions.data)
 
-# pass the nft and exhibition id to this function
-def register_artwork_for_exhibition(pk):
-
-    mydata = NFT.objects.get(id=pk)
-
-    print(mydata.get_nft_exhibition_status())
-
-    if mydata.get_nft_exhibition_status() is None:
-        new = models.NFtEx.objects.create(nft=models.NFT.objects.get(id=2), ex=models.Exhibition.objects.get(id=1),
-                                          commission=4, date=datetime.datetime.now,
-                                          is_nft_viewed_by_exhibitor=False, is_nft_accepted_by_exhibitor=False)
-        new.save()
-    else:
-        print("error")
-
-
-# Run func below to add a nft to an exhibition
-# register_artwork_for_exhibition()    
 
 
 # Pass the artwork id to change is_accepted IF EXHIBITOR CONFIRM THE REQUEST
-def check_register_artwork_by_exhibitor(pk):
-    new = models.NFtEx.objects.get(id=pk)
-    new.is_nft_accepted_by_exhibitor = True
-    new.save()
+# def check_register_artwork_by_exhibitor(pk):
+#     new = models.NFtEx.objects.get(id=pk)
+#     new.is_nft_accepted_by_exhibitor = True
+#     new.save()
 
 
 
 # Pass the artwork id to check IF EXHIBITOR HAS SEEN THE REQUEST OR NOT
-def check_request_seen_by_exhibitor(pk):
-    new = models.NFtEx.objects.get(id=pk)
-    new.is_nft_viewed_by_exhibitor = True
-    new.save()
+# def check_request_seen_by_exhibitor(pk):
+#     new = models.NFtEx.objects.get(id=pk)
+#     new.is_nft_viewed_by_exhibitor = True
+#     new.save()
 
 
 # Run func below when exhibitor SEEN the NFT request
