3d5a1abc89c82694ca567d5070046a675993b8b3
jestemAria <radmehr.krash@gmail.com>
2022-09-06 05:05:52 +0430

    settings.py updated with db_test path

--- ARTINA/settings.py
+++ ARTINA/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': BASE_DIR / 'documentation/db_test.sqlite3',
+        'NAME': BASE_DIR / 'documentation/database/db_test.sqlite3',
     }
 }
 
