72d6011cf532dd24257f4f5d0b17f416a6be6210
alikhazaei <alikhazaee00@gmail.com>
2022-09-11 17:36:23 +0430

    url for rate added

--- Account/urls.py
+++ Account/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register(r'artists',views.ArtistViewSet)
+router.register(r'rate',views.ArtistRateViewSet)
 
 urlpatterns=[
     path('',include(router.urls))
