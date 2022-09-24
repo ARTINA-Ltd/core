0c4d298d07e5bf0de33eb672c47562f56e05f5c5
mohammad <enjoy4everyday@gmail.com>
2022-09-24 10:53:03 +0330

    connect online and test

--- solidity/nft/hardhat.config.ts
+++ solidity/nft/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
+import { ripemd160 } from "ethers/lib/utils";
+
+const RINKEBY_URL = process.env.RINKEBY_URL as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.11",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- solidity/readme.txt
+++ solidity/readme.txt
@@ -9,6 +9,8 @@ npx hardhat
 
 yarn add --dev @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers ts-node @types/mocha
 yarn add @openzeppelin/contracts
+yarn add dotenv
+
 
 to check : 
 npx hardhat test
@@ -21,4 +23,17 @@ yarn add --dev (name of the library) like @openzeppelin/contracts
 
 run whole things:
 yarn && yarn dev
+
+
+website:
+https://infura.io/
+
+env:
+RINKEBY_URL=
+PRIVATE_KEY=
+
+NEXT_PUBLIC_NFT_MARKET_ADDRESS=
+NEXT_PUBLIC_GRAPH_URL=
+
+NFT_STORAGE_KEY=
 
