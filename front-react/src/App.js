8c86ce04b06fcf28480b0cdd6eb8c9f9e98983a2
jestemAria <radmehr.krash@gmail.com>
2022-12-20 05:57:12 +0330

    connectWallet config and button added

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -5,30 +5,39 @@ import HomePage from "./Pages/HomePage";
 import ArtistApplicationForm from "./Pages/ExhibitionSignForm";
 import NFTDetails from "./Pages/ProductPage";
 import Login from "./Pages/LoginPage";
-import Register from "./LoginComponent/SignUp";
+// import Register from "./LoginComponent/SignUp";
+import Register from "./LoginComponent/register";
 import ExhibitorPage from "./Pages/Exhabition";
 import ExhibitionLists from './Pages/Exhibition_lists'
 import RequestDetails from "./ExhabitionComponent/src/component/request-artist-detail-page/r-page/request-detail";
 import ShowRequests from "./ExhabitionComponent/src/component/request-artist-detail-page/r-page/show-req";
 import RequestLists from "./ExhabitionComponent/src/component/requestlistforexhibition/requests/paging";
 // eslint-disable-next-line import/no-anonymous-default-export
+
+import {ChainId, ThirdwebProvider} from "@thirdweb-dev/react";
+
+const activeChainId = ChainId.Goerli;
+
+
 export default () => {
     return <>
-        <div className="App">
-            <Routes>
-                <Route  path="/" element={<StarterFile />}/>
-                <Route exact path="artist-page" element={<ArtistPage />} />
-                <Route exact path="homepage" element={<HomePage />} />
-                <Route exact path="artist-application-form" element={<ArtistApplicationForm />} />
-                <Route exact path="nft-details" element={<NFTDetails />} />
-                <Route exact path="login" element={<Login />} />
-                <Route exact path="register" element={<Register />} />
-                <Route exact path="exhibitor-page" element={<ExhibitorPage />} />
-                <Route exact path="exhibition-lists" element={<ExhibitionLists />} />
-                <Route exact path="request-lists" element={<RequestLists/>} />
-                <Route exact path="show-request/" element={<ShowRequests/>} />
-                <Route exact path="request-details" element={<RequestDetails/>} />
-            </Routes>
-        </div>
+        <ThirdwebProvider desiredChainId={activeChainId}>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<StarterFile/>}/>
+                    <Route exact path="artist-page" element={<ArtistPage/>}/>
+                    <Route exact path="homepage" element={<HomePage/>}/>
+                    <Route exact path="artist-application-form" element={<ArtistApplicationForm/>}/>
+                    <Route exact path="nft-details" element={<NFTDetails/>}/>
+                    <Route exact path="login" element={<Login/>}/>
+                    <Route exact path="register" element={<Register/>}/>
+                    <Route exact path="exhibitor-page" element={<ExhibitorPage/>}/>
+                    <Route exact path="exhibition-lists" element={<ExhibitionLists/>}/>
+                    <Route exact path="request-lists" element={<RequestLists/>}/>
+                    <Route exact path="show-request/" element={<ShowRequests/>}/>
+                    <Route exact path="request-details" element={<RequestDetails/>}/>
+                </Routes>
+            </div>
+        </ThirdwebProvider>
     </>
 };
