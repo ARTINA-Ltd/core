6de44d8e9ce95c467a34573c7192ca1cab9da7e1
jestemAria <radmehr.krash@gmail.com>
2022-12-25 03:57:53 +0330

    Dropzone component has got some changes in logic and style

--- front-react/src/components/Dropzone.jsx
+++ front-react/src/components/Dropzone.jsx
@@ -1,44 +1,59 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
 import {useDropzone} from "react-dropzone";
 import "./Dropzone.css";
+import cuid from "cuid";
 
 function Dropzone({onDrop, accept, open}) {
+    const [myFiles, setMyFiles] = useState([]);
+
+    // const onDrop = useCallback(acceptedFiles => {
+    //     setMyFiles([...myFiles, ...acceptedFiles])
+    // }, [myFiles])
+
     const {getRootProps, getInputProps, isDragActive, acceptedFiles} =
         useDropzone({
             accept,
             onDrop,
         });
 
+    const removeFile = file => () => {
+        console.log("removeFile", file);
+        const newFiles = [...myFiles]
+        newFiles.splice(newFiles.indexOf(file), 1)
+        setMyFiles(newFiles)
+    }
+
     const files = acceptedFiles.map((file) => (
         <li key={file.path}>
-            {file.path} - {file.size} bytes
+            {file.path} - {file.size} bytes{" "}
+            <button className="remove-btn" onClick={removeFile(file)}>x</button>
         </li>
     ));
 
 
     return (
-        <div>
+        <div className="main-div">
             <div {...getRootProps({className: "dropzone"})}>
                 <input className="input-zone" {...getInputProps()} />
                 <div className="text-center">
                     {isDragActive ? (
                         <p className="dropzone-content">
-                            فایل خود را اینجا ول کنید
+                            فایل خود را اینجا رها کنید
                         </p>
                     ) : (
                         <p className="dropzone-content">
-                            فایل خود را اینجا ول کنید و یا کلیک کنید
+                            فایل خود را اینجا رها کنید و یا کلیک کنید
                         </p>
                     )}
                     <button type="button" onClick={open} className="btn">
-                        Click to select files
+                        برای انتخاب فایل کلیک کنید
                     </button>
                 </div>
             </div>
-            re
             <aside>
                 <ul>{files}</ul>
             </aside>
+            {/*{files.length > 0 && <button className="remove-btn" onClick={removeAll}>x</button>}*/}
         </div>
     );
 }
