32582638e3bcb416743a07f9af5dc555e2398ef7
parsa k <parsawriter1999@gmail.com>
2023-02-15 11:56:47 +0330

    changes profile grid and components

--- front-react/src/Pages/ProfilePage.js
+++ front-react/src/Pages/ProfilePage.js
@@ -1,33 +1,31 @@
 import Image from "../ProfilePage/image";
 import PersonalInfo from "../ProfilePage/PersonalInfo";
 import Navbar from "../ProfilePage/nav-bar";
- import OtherInfo from "../ProfilePage/OtherInfo";
+import OtherInfo from "../ProfilePage/OtherInfo";
 import Autinticate from "../ProfilePage/Autinticate";
-import './ProfilePage.css'
-import Header from "../components/LandingPageNavBar/Header";
+ import Header from "../components/LandingPageNavBar/Header";
 import Footer from "../components/Footer/Footer";
+import Profileuploader from "../components/Uploaders/Profileuploader";
+import IDUpdate from "../components/Uploaders/IDUpdate";
+ 
 function ProfilePage() {
   return (
-    <div className="  overflow-hidden grid " style={{direction:'rtl'}}>
+    <div className="  overflow-hidden grid " style={{ direction: "rtl" }}>
       <Header />
-      <div dir="rtl" className="flex grid col-12 s1 m-2">
-        <div className="lg:col-6 md:col-6 sm:col-12 mt-6">
-        <Image />
+      <h2 className="  mt-6 lg:text-8xl sm:text-4xl lg:text-6xl text-4xl justify-content-center text-center">مشخصات شخصی</h2>
+
+      <div dir="rtl" className="flex grid col-12  m-2">
+        <div className="lg:col-6     md:col-6 sm:col-12 mt-6">
+          <Profileuploader />
+          <IDUpdate />
         </div>
         <div className="lg:col-6 md:col-6  sm:col-12 mt-2">
-        <PersonalInfo />
-
+          <PersonalInfo />
         </div>
       </div>
-      <div className="col-12  otherinfo"  >
-      <OtherInfo />
+       
+     
 
-      </div>
-      <div className="col-12 m-2" style={{marginTop:'99px'}}>
-      <Autinticate />
-
-      </div>
-        
       <Footer />
     </div>
   );
