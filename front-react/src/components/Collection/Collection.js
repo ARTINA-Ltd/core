8128b7a8afd963b1ffed04af3a923de20bd05097
parsa k <parsawriter1999@gmail.com>
2023-02-19 15:01:08 +0330

    collectionPopup has been added

--- front-react/src/components/Collection/Collection.js
+++ front-react/src/components/Collection/Collection.js
@@ -4,12 +4,16 @@ import { DataView, DataViewLayoutOptions } from "primereact/dataview";
 import { Skeleton } from "primereact/skeleton";
 import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
+import { Rating } from "primereact/rating";
+import { Tag } from "primereact/tag";
+import ImageBG from "../../HomePageComponent/ArtNft3.jpg";
 
+import CollectionDialog from "../Dialog/CollectionDialog/CollectionDialog";
 export default function BasicDemo() {
   const [products, setProducts] = useState([
     {
       id: 0,
-      image: "",
+      image: require("../../HomePageComponent/ArtNft3.jpg"),
       name: "this is name",
       basicprice: "9000",
     },
@@ -26,30 +30,31 @@ export default function BasicDemo() {
       basicprice: "9000",
     },
     {
-      id: 2,
+      id: 3,
       image: "",
       name: "this is name",
       basicprice: "9000",
     },
     {
-      id: 2,
+      id: 4,
       image: "",
       name: "this is name",
       basicprice: "9000",
     },
     {
-      id: 2,
+      id: 5,
       image: "",
       name: "this is name",
       basicprice: "9000",
     },
     {
-      id: 2,
+      id: 6,
       image: "",
       name: "this is name",
       basicprice: "9000",
     },
   ]);
+ 
   const [sortKey, setSortKey] = useState("");
   const [sortOrder, setSortOrder] = useState(0);
   const [sortField, setSortField] = useState("");
@@ -57,52 +62,141 @@ export default function BasicDemo() {
     { label: "قیمت زیاد به کم", value: "!price" },
     { label: "قیمت کم به زیاد", value: "price" },
   ];
+ 
   const [layout, setLayout] = useState("grid");
-
-  useEffect(() => {
+  const [loading, setloading] = useState(false);
+   useEffect(() => {
     // ProductService.getProductsSmall().then((data) => setProducts(data.slice(0, 6)));
   }, []);
 
   const listItem = () => {
+    if (loading) {
+      return (
+        <div className="col-12">
+          <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+            <Skeleton className="w-9 sm:w-16rem xl:w-10rem shadow-2 h-6rem block xl:block mx-auto border-round" />
+            <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+              <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                <Skeleton className="w-8rem border-round h-2rem" />
+                <Skeleton className="w-6rem border-round h-1rem" />
+                <div className="flex align-items-center gap-3">
+                  <Skeleton className="w-6rem border-round h-1rem" />
+                  <Skeleton className="w-3rem border-round h-1rem" />
+                </div>
+              </div>
+              <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                <Skeleton className="w-4rem border-round h-2rem" />
+                <Skeleton shape="circle" className="w-3rem h-3rem" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+         
+
     return (
-      <div className="col-12">
+      <>
+       
+        <div className="col-12">
         <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-          <Skeleton className="w-9 sm:w-16rem xl:w-10rem shadow-2 h-6rem block xl:block mx-auto border-round" />
+       
+            <img
+              className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"
+              src={products[0].image}
+              alt={products[0].name}
+            />;
+        
+
           <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
             <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-              <Skeleton className="w-8rem border-round h-2rem" />
-              <Skeleton className="w-6rem border-round h-1rem" />
+              <div className="text-2xl font-bold text-900">{products[0].name}s</div>
+              <Rating value={products[0].rating} readOnly cancel={false}></Rating>
               <div className="flex align-items-center gap-3">
-                <Skeleton className="w-6rem border-round h-1rem" />
-                <Skeleton className="w-3rem border-round h-1rem" />
+                <span className="flex align-items-center gap-2">
+                  <i className="pi pi-tag"></i>
+                  <span className="font-semibold">{products[0].category}</span>
+                </span>
+                <Tag
+                  value={products[0].inventoryStatus}
+                  severity={getSeverity(products[0])}
+                ></Tag>
               </div>
             </div>
             <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+              <span className="text-2xl font-semibold">${products[0].basicprice}</span>
+              <CollectionDialog    />
+              {/* <Button
+               
+                className=" font" onClick={()=>{setVisible(true)}}
+                disabled={products.inventoryStatus === "OUTOFSTOCK"}
+              >فروش</Button> */}
+            </div>
+          </div>
+        </div>
+      </div> 
+     
+      </>
+     
+     
+    );
+  };
+
+  const gridItem = () => {
+    if (loading) {
+      return (
+        <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+          <div className="p-4 border-1 surface-border surface-card border-round">
+            <div className="flex flex-wrap align-items-center justify-content-between gap-2">
+              <Skeleton className="w-6rem border-round h-1rem" />
+              <Skeleton className="w-3rem border-round h-1rem" />
+            </div>
+            <div className="flex flex-column align-items-center gap-3 py-5">
+              <Skeleton className="w-9 shadow-2 border-round h-10rem" />
+              <Skeleton className="w-8rem border-round h-2rem" />
+              <Skeleton className="w-6rem border-round h-1rem" />
+            </div>
+            <div className="flex align-items-center justify-content-between">
               <Skeleton className="w-4rem border-round h-2rem" />
               <Skeleton shape="circle" className="w-3rem h-3rem" />
             </div>
           </div>
         </div>
-      </div>
-    );
-  };
+      );
+    }
 
-  const gridItem = () => {
     return (
       <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
         <div className="p-4 border-1 surface-border surface-card border-round">
           <div className="flex flex-wrap align-items-center justify-content-between gap-2">
-            <Skeleton className="w-6rem border-round h-1rem" />
-            <Skeleton className="w-3rem border-round h-1rem" />
+            <div className="flex align-items-center gap-2">
+              <i className="pi pi-tag"></i>
+              <span className="font-semibold">{products[0].category}</span>
+            </div>
+            <Tag
+              value={products[0].inventoryStatus}
+              severity={getSeverity(products)}
+            ></Tag>
           </div>
           <div className="flex flex-column align-items-center gap-3 py-5">
-            <Skeleton className="w-9 shadow-2 border-round h-10rem" />
-            <Skeleton className="w-8rem border-round h-2rem" />
-            <Skeleton className="w-6rem border-round h-1rem" />
+            <img
+              className="w-9 shadow-2 border-round"
+              src={ImageBG}
+              alt={products.name}
+            />
+            <div className="text-2xl font-bold">{products[0].name}</div>
+            <Rating value={products.rating} readOnly cancel={false}></Rating>
           </div>
           <div className="flex align-items-center justify-content-between">
-            <Skeleton className="w-4rem border-round h-2rem" />
-            <Skeleton shape="circle" className="w-3rem h-3rem" />
+          <span className="text-2xl font-semibold">${products[0].basicprice}</span>
+          <CollectionDialog    />
+
+            {/* 
+            <Button
+              icon="pi pi-shopping-cart"
+              className="font"
+              disabled={products.inventoryStatus === "OUTOFSTOCK"}
+            >فروش</Button> */}
           </div>
         </div>
       </div>
@@ -122,29 +216,30 @@ export default function BasicDemo() {
     return (
       <div className=" flex flex-wrap ">
         <div className="  flex   ">
-         
-          <div  className="mr-2" style={{direction:'ltr',}}>
+          <div className="mr-2" style={{ direction: "ltr" }}>
             <DataViewLayoutOptions
-            
               layout={layout}
               onChange={(e) => setLayout(e.value)}
             />
           </div>
-          <div className="mr-3  " >
-            <Button icon="pi pi-refresh" style={{width:'40px'}} rounded raised />
-          </div>  
+          <div className="mr-3  ">
+            <Button
+              icon="pi pi-refresh"
+              style={{ width: "40px" }}
+              rounded
+              raised
+            />
+          </div>
         </div>
-        <div className="      mt-3 mr-8  "  >
+        <div className="      mt-3 mr-8  ">
           <Dropdown
             options={sortOptions}
             value={sortKey}
-            
             optionLabel="label"
             placeholder="  مرتب کردن قیمت"
             onChange={onSortChange}
             className="w-full sm:w-14rem font"
           />
-          
         </div>
       </div>
     );
