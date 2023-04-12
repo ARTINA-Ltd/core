3dacaec8989250bf8fd910289d91e8492432c1fd
Hosein Afsharchi <mhafsharchi@gmail.com>
2023-04-12 18:28:59 +0430

    اصلاح لینک های هدر

--- front-react/src/components/LandingPageNavBar/Header.js
+++ front-react/src/components/LandingPageNavBar/Header.js
@@ -5,82 +5,179 @@ import { Avatar } from "primereact/avatar";
 import { useNavigate } from "react-router";
 
 const Header = () => {
-  // const state = {clicked: false}
   const [clicked, setClicked] = useState(false);
   const Token = localStorage.getItem("authTokens");
   const navigate = useNavigate();
 
   const handleClick = () => {
-    setClicked(!clicked);
+    setClicked((e) => !e);
   };
 
+  const NotActiveItems = [
+    {
+      title: 'درباره‌ما',
+      link: '/'
+    },
+    {
+      title: 'پشتیبانی',
+      link: '/'
+    },
+    {
+      title: 'بلاگ',
+      link: '/'
+    }
+  ]
+
+  const ActiveItems = [
+    {
+      title: 'داشبورد',
+      link: '/'
+    },
+    {
+      title: 'درخواست‌ها',
+      link: '/'
+    },
+    {
+      title: 'درباره‌ما',
+      link: '/'
+    },
+    {
+      title: 'پشتیبانی',
+      link: '/'
+    },
+    {
+      title: 'بلاگ',
+      link: '/'
+    },
+    {
+      title: 'کارمزد',
+      link: '/'
+    },
+    {
+      title: 'کانکت والت',
+      link: '/'
+    }
+
+  ]
   const ActiveUser = () => {
     return (
+      // <>
+      //   <header className="home-header flex ">
+      //     {/*<h2>Inc. This Morning</h2>*/}
+      //     <div className="navbar-menu p-2 w-full justify-content-start ">
+      //       <div className="navbar-link-logo " onClick={handleClick}>
+      //         <i
+      //           className={clicked ? "pi  pi-times" : " pi pi-bars "}
+      //           style={{ fontSize: "2rem" }}
+      //         ></i>
+      //       </div>
+      //       <div className=" justify-content-canter">
+      //         <ul
+      //           className={
+      //             clicked
+      //               ? "navbar-item active w-full mt-8 p-2"
+      //               : "w-full navbar-item justify-content-canter"
+      //           }
+      //           style={{ fontSize: "2rem" }}
+      //         >
+      //           <a href="" className="navbar-link">
+      //             {" "}
+      //           </a>
+      //           <a href="" className="navbar-link-exp font">
+      //             خانه
+      //           </a>
+      //           <a href="" className="navbar-link-exp font p-2">
+      //             درباره{" "}
+      //           </a>
+      //           <a href="" className="navbar-link-exp  font">
+      //             پشتیبانی
+      //           </a>
+      //           <a href="" className="navbar-link-exp font">
+      //             پشتیبانی
+      //           </a>
+
+      //           <a
+      //             href=""
+      //             className="navbar-link-1 font"
+      //             onClick={() => {
+      //               navigate("/login");
+      //               localStorage.setItem("authTokens", null);
+      //             }}
+      //           >
+      //             {" "}
+      //             خروج{" "}
+      //           </a>
+      //         </ul>
+      //       </div>
+      //       <div className="navbar-menue  w-full   justify-content-end  ">
+      //         <a
+      //           href=""
+      //           className="navbar-link-2  font"
+      //           style={{ color: "#424874" }}
+      //           onClick={() => {
+      //             navigate("/login");
+      //             localStorage.setItem("authTokens", null);
+      //           }}
+      //         >
+      //           {" "}
+      //           خروج{" "}
+      //         </a>
+      //         <div className="navbar-logo        ">
+      //           <img src={Logo} alt="logo" />
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </header>
+      // </>
       <>
-        <header className="home-header flex ">
+        <header>
           {/*<h2>Inc. This Morning</h2>*/}
-          <div className="navbar-menu p-2 w-full justify-content-start ">
+          <div className="navbar-menu flex justify-center text-[16px]">
             <div className="navbar-link-logo " onClick={handleClick}>
               <i
                 className={clicked ? "pi  pi-times" : " pi pi-bars "}
                 style={{ fontSize: "2rem" }}
               ></i>
             </div>
-            <div className=" justify-content-canter">
-              <ul
+            <div className="flex items-center justify-end lg:justify-between w-[90%] justify-self-center lg:w-[80%]">
+              <div
                 className={
                   clicked
                     ? "navbar-item active w-full mt-8 p-2"
-                    : "w-full navbar-item justify-content-canter"
+                    : "w-full hidden gap-8 lg:flex"
                 }
-                style={{ fontSize: "2rem" }}
               >
-                <a href="" className="navbar-link">
-                  {" "}
-                </a>
-                <a href="" className="navbar-link-exp font">
-                  خانه
-                </a>
-                <a href="" className="navbar-link-exp font p-2">
-                  درباره{" "}
-                </a>
-                <a href="" className="navbar-link-exp  font">
-                  پشتیبانی
-                </a>
-                <a href="" className="navbar-link-exp font">
-                  پشتیبانی
-                </a>
 
-                <a
-                  href=""
-                  className="navbar-link-1 font"
+                {ActiveItems.map((item) => (
+
+
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => {
+                      navigate(item.link);
+                    }}
+                  >
+                    {item.title}
+                  </div>
+                ))}
+
+
+              </div>
+
+              <div className="flex gap-5 items-center">
+                <div
+                  className="cursor-pointer"
                   onClick={() => {
                     navigate("/login");
                     localStorage.setItem("authTokens", null);
                   }}
                 >
-                  {" "}
-                  خروج{" "}
-                </a>
-              </ul>
-            </div>
-            <div className="navbar-menue  w-full   justify-content-end  ">
-              <a
-                href=""
-                className="navbar-link-2  font"
-                style={{ color: "#424874" }}
-                onClick={() => {
-                  navigate("/login");
-                  localStorage.setItem("authTokens", null);
-                }}
-              >
-                {" "}
-                خروج{" "}
-              </a>
-              <div className="navbar-logo        ">
+                  خروج
+                </div>
                 <img src={Logo} alt="logo" />
               </div>
             </div>
+
           </div>
         </header>
       </>
@@ -90,69 +187,53 @@ const Header = () => {
   const NoActiveUser = () => {
     return (
       <>
-        <header className="home-header flex ">
+        <header>
           {/*<h2>Inc. This Morning</h2>*/}
-          <div className="navbar-menu p-2 w-full justify-content-start ">
+          <div className="navbar-menu flex justify-center text-[16px]">
             <div className="navbar-link-logo " onClick={handleClick}>
               <i
                 className={clicked ? "pi  pi-times" : " pi pi-bars "}
                 style={{ fontSize: "2rem" }}
               ></i>
             </div>
-            <div className=" justify-content-canter">
-              <ul
+            <div className="flex items-center justify-end lg:justify-between w-[90%] justify-self-center lg:w-[80%]">
+              <div
                 className={
                   clicked
                     ? "navbar-item active w-full mt-8 p-2"
-                    : "w-full navbar-item justify-content-canter"
+                    : "w-full hidden gap-8 lg:flex"
                 }
-                style={{ fontSize: "2rem" }}
               >
-                <a href="" className="navbar-link">
-                  {" "}
-                </a>
-                <a href="" className="navbar-link-exp font">
-                  خانه
-                </a>
-                <a href="" className="navbar-link-exp font p-2">
-                  درباره{" "}
-                </a>
-                <a href="" className="navbar-link-exp  font">
-                  پشتیبانی
-                </a>
-                <a href="" className="navbar-link-exp font">
-                  پشتیبانی
-                </a>
-                {/*</ul>*/}
-                {/*<ul className="navbar-item-1">*/}
-                <a
-                  href=""
-                  className="navbar-link-1 font"
+
+                {NotActiveItems.map((item) => (
+
+
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => {
+                      navigate(item.link);
+                    }}
+                  >
+                    {item.title}
+                  </div>
+                ))}
+
+
+              </div>
+
+              <div className="flex gap-5 items-center">
+                <div
+                  className="cursor-pointer"
                   onClick={() => {
                     navigate("/login");
                   }}
                 >
-                  {" "}
-                  ورود{" "}
-                </a>
-              </ul>
-            </div>
-            <div className="navbar-menue  w-full   justify-content-end  ">
-              <a
-                href=""
-                className="navbar-link-2  font"
-                style={{ color: "#424874" }}
-                onClick={() => {
-                  navigate("/login");
-                }}
-              >
-                {" "}
-                ورود{" "}
-              </a>
-              <div className="navbar-logo        ">
+                  ورود
+                </div>
                 <img src={Logo} alt="logo" />
               </div>
             </div>
+
           </div>
         </header>
       </>
