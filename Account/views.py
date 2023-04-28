d0ab7b9f1e59ce3d36a83558e869442638963cf1
zehish <40887905+zehish@users.noreply.github.com>
2023-04-28 10:14:23 +0330

    adding profile update
    using partial for more flexibility

--- Account/views.py
+++ Account/views.py
@@ -217,3 +217,17 @@ class ArtistRateViewSet(viewsets.ModelViewSet):
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
