5045d57af21d459ecf75bbdeaa4d26590a31bd1f
jestemAria <radmehr.krash@gmail.com>
2023-05-06 11:19:44 +0330

    localhost -> ip

--- front-react/src/LoginComponent/login.jsx
+++ front-react/src/LoginComponent/login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:8000/api/account/login/", {
+        const response = await fetch("http://78.38.35.249/api/account/login/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
