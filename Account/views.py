a50989dafbd49ef1b7ad858fd5529e099f7f3b44
jestemAria <radmehr.krash@gmail.com>
2023-05-12 14:45:54 +0330

    profileViewSet updated

--- Account/views.py
+++ Account/views.py
@@ -102,11 +102,8 @@ class ProfileViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
-    def perform_update(self, serializer):
         serializer.save()
+        return Response(serializer.data)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
