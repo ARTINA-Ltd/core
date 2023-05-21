ea969b13ab3cfa238d54dc2ca2b96bd6617bc5a0
zehi <you@example.com>
2023-05-22 01:52:30 +0330

    error handeling

--- Account/views.py
+++ Account/views.py
@@ -324,7 +324,9 @@ class UserBalanceViewSet(viewsets.ModelViewSet):
         
         balance = {
             'rial_available_balance': user_balance.rial_available_balance,
+            'rial_unavailable_balance': user_balance.rial_unavailable_balance,
             'eth_balance': user_balance.eth_balance,
+            
             # Add other balance fields as needed
         }
 
