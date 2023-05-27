5660629b9842ac45703c6612b982fe5b7cfc2a9f
jestemAria <radmehr.krash@gmail.com>
2023-05-28 00:21:46 +0330

    register is now responsive

--- front-react/src/components/Home/MainSlider.jsx
+++ front-react/src/components/Home/MainSlider.jsx
@@ -8,7 +8,7 @@ const MainSlider = () => {
   });
 
   return (
-    <div className="w-full h-[700px] keen-slider" ref={sliderRef}>
+    <div className="w-full h-[700px] keen-slider lg:h-[550px] md:h-[400px]" ref={sliderRef}>
       <div className="keen-slider__slide w-full h-full">
         <img src="/1.jpg" className="object-cover w-full" alt="" />
       </div>
