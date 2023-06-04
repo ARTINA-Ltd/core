33030d1843cfd7b9da0c2c725e24a59ef5e302f1
zehi <you@example.com>
2023-06-04 21:58:17 +0330

    typo fixing

--- core/views.py
+++ core/views.py
@@ -58,7 +58,7 @@ class OrderViewSet(viewsets.ViewSet):
             #         return Response(status=HTTPStatus.BAD_REQUEST)
     def list(self, request, *args, **kwargs):
         token_id = request.data.get('token_id')
-            if not token_id:
+        if not token_id:
             return Response({'error': 'Token ID not provided in header'}, status=HTTPStatus.BAD_REQUEST)
         
         nft = NFT.objects.filter(token_id=token_id).first()
