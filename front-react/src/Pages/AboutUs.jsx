43c19473e3e168f4a04ee15c28c5540a3c860ae2
jestemAria <radmehr.krash@gmail.com>
2023-06-24 22:03:09 +0330

    AboutUs component added to the repository

---
+++ front-react/src/Pages/AboutUs.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import SimpleCard from "../components/Cards/UserDashboardCards/SimpleCard";
+import TestLayout from "../Layouts/TestLayout";
+
+const Contact = () => {
+    return (
+        <TestLayout>
+            <div className="w-[55%] m-auto lg:w-4/5 md:w-11/12">
+                <SimpleCard className={'text-center bg-white leading-[40px]'}>
+                    <div className="text-[32px] mb-5">درباره ما</div>
+                    <div className="text-[18px] mb-7 text-center">
+                        آرتینا اولین بازار ساخت، خرید و فروش و نمایش nft در متاورس با پول رایج ایرانی در دنیاست.
+                    </div>
+                    <div className="text-[25px] mb-2">محصول آرتینا</div>
+                    <div className="text-[18px] mb-7 text-justify px-6">
+                        آرتینا یک پلتفرم خرید و فروش آثار هنری  در قالب nft با رویکرد توانمند سازی هنرمندان برای نمایش گذاشتن و خرید و فروش آثارشان است. این محصول برای نمایشگاه دارها و هنرمندان امکان ایجاد نمایشگاه های مجازی و خرید و فروش با رمز ارز و تومان را فراهم کرده است.
+                    </div>
+                    <div className="text-[25px] mb-2">هدف</div>
+                    <div className="text-[18px] mb-7 px-6">
+                        فراهم نمودن بستری امن، سریع و آسان جهت خرید و فروش مستقیم nft بین خریدار و فروشنده است.
+                    </div>
+                    <div className="text-[25px] mb-2">شکل گیری تیم آرتینا</div>
+                    <div className="mt-6 text-[18px] text-justify px-6">
+                        آرتینا با هدف رفع نیاز کاربران ایرانی برای بازار nft در بهار 1401 شکل گرفت. پس از تحقیقات اولیه و سنجش میزان نیاز بازار ایران به چنین بازاری، هسته اولیه تیم در مرکز رشد دانشگاه زنجان استقرار یافت. به تدریج و با افزوده شدن نفراتی با تخصص‌های مختلف، تیم بزرگ‌تر شد.
+                    </div>
+                </SimpleCard>
+            </div>
+        </TestLayout>
+    );
+};
+
+export default Contact;
