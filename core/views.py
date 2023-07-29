481ec6efb1d954b78c5bf1b66996d8d9a1d87a55
zehi <you@example.com>
2023-07-29 21:11:18 +0330

    changing most_liked

--- core/views.py
+++ core/views.py
@@ -191,7 +191,7 @@ class NFTRatingViewSet(viewsets.ModelViewSet):
         # Serialize the NFTs and add the like count to each one
         serialized_nfts = []
         for nft in most_liked_nfts:
-            serialized_nft = serializers.NFTSerializer.get_serializer(nft).data
+            serialized_nft = serializers.NFTSerializer(nft).data
             serialized_nft['like_count'] = nft.like_count
             serialized_nfts.append(serialized_nft)
         
