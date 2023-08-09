f015d9b8ccec11bd4ed7059da58ed3defec007df
jestemAria <radmehr.krash@gmail.com>
2023-08-09 15:26:40 +0330

    the page `help-create-exhibition` completed

--- front-react/src/Pages/help-create-exhibition.jsx
+++ front-react/src/Pages/help-create-exhibition.jsx
@@ -9,13 +9,41 @@ const HelpCreateExhibition = () => {
                 <SimpleCard className={'text-center bg-white leading-[40px]'}>
                     <div className="text-[32px] mb-5">راهنمای ایجاد نمایشگاه</div>
                     <div className="text-[18px] mb-4 text-justify px-6">
-                        ۱- ابتدا در آرتینا رجیستر و سپس لاگین کنید.
+                        ۱- ابتدا وارد اکانت خود شوید.
                     </div>
                     <div className="text-[18px] mb-4 text-justify px-6">
-                        ۲- سپس اثرهای خود در آرتینا ضرب کنید.
+                        ۲- سپس آثار خود در آرتینا ضرب کنید. چنانجه هنوز اثری را ضرب نکرده اید، می‌توانید
+                        <a className="text-[18px] mb-4 text-justify text-purple-700" href="https://artina.org/help-create-wallet"> راهنمای ضرب ان اف تی </a>
+                        را مشاهده کنید.
                     </div>
                     <div className="text-[18px] mb-4 text-justify px-6">
-                        ۳- در بخش فعالیت ها وارد ایجا نمایشگاه شوید.
+                        ۳- از نوار بالای سایت وارد بخش «مدیریت فعالیت ها» شوید؛ سپس از بخش «نمایشگاه‌های در حال برگزاری شما» بر روی افزودن نمایشگاه کلیک کنید.
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        ۴-   اطلاعات مورد نیاز را با توجه به اثر خود تکمیل کنید؛ لازم به ذکر است که پر کردن تمامی مقادیر در صفحه ساخت نمایشگاه اجباری می‌باشد. 
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        ✔️ تصویر زیر نمونه ای از ایجاد نمایشگاه را نمایش می‌دهد.
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        <img
+                            src="/help-exhibition-1.png"
+                            className=" object-cover m-auto max-w-md"
+                            alt=""
+                        />
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        ۵- در نهایت با موافقت قوانین ساخت نمایشگاه، بر روی دکمه ثبت کلیک کنید.
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        ۶- در نهایت می‌توانید نمایشگاه خود را در بخش اصلی مدیریت فعالیت ها مشاهده کنید. 🎉
+                    </div>
+                    <div className="text-[18px] mb-4 text-justify px-6">
+                        <img
+                            src="/help-exhibition-2.png"
+                            className=" object-cover m-auto max-w-xs"
+                            alt=""
+                        />
                     </div>
                 </SimpleCard>
             </div>
