131d574600104738a1c6444e18e01d63c45f4154
jestemAria <radmehr.krash@gmail.com>
2023-08-10 20:15:26 +0330

    commission components changed to table form

--- front-react/src/Pages/Commission/CommissionComponents/Commissionmax.js
+++ front-react/src/Pages/Commission/CommissionComponents/Commissionmax.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
 export default function Commissionmax() {
   return (
-    <div>
-      <div className=" m-8">
-        <div className="Row1  flex col-12 ">
-          <div className="col1 col-2 py-2 -3xl " style={{backgroundColor:"#424874",color:"white"}}>حداقل قابل واریز</div>
-          <div className="col2 col-2 py-2 -3xl" style={{backgroundColor:"#424874",color:"white"}}>هزینه واریز</div>
-          <div className="col1 col-2 py-2 -3xl" style={{backgroundColor:"#424874",color:"white"}}>حداقل قابل برداشت</div>
-          <div className="col2 col-3 py-2 -3xl" style={{backgroundColor:"#424874",color:"white"}}>هزینه قابل برداشت</div>
-          <div className="col2 col-3 py-2 -3xl" style={{backgroundColor:"#424874",color:"white"}}>دارایی دیجیتال</div>
-        </div>
-        <div className="Row1  flex col-12">
-          <div className="col1 col-2 py-2 -2xl">15 USDT</div>
-          <div className="col2 col-2 py-2 -2xl">10 USDT</div>
-          <div className="col1 col-2 py-2 -2xl">500 USDT</div>
-          <div className="col2 col-3 py-2 -2xl">متغییر بنا به شبکه تتر</div>
-          <div className="col2 col-3 py-2 -2xl">شبکه اتریوم</div>
-        </div>
-        <div className="Row2  flex col-12">
-          <div className="col1 col-2 py-2 -2xl">0.5 USDT</div>
-          <div className="col2 col-2 py-2 -2xl">0 USDT</div>
-          <div className="col1 col-2 py-2 -2xl">10 USDT</div>
-          <div className="col2 col-3 py-2 -2xl">؟</div>
-          <div className="col2 col-3 py-2 -2xl">شبکه ترون</div>
-        </div>
-      </div>
+    <div className="m-8">
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 bg-indigo-700 text-white">دارایی دیجیتال</th>
+            <th className="py-2 px-4 bg-indigo-700 text-white">حداقل قابل واریز</th>
+            <th className="py-2 px-4 bg-indigo-700 text-white">هزینه واریز</th>
+            <th className="py-2 px-4 bg-indigo-700 text-white">حداقل قابل برداشت</th>
+            <th className="py-2 px-4 bg-indigo-700 text-white">هزینه قابل برداشت</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="py-2 px-4 bg-indigo-300">شبکه اتریوم</td>
+            <td className="py-2 px-4">15 USDT</td>
+            <td className="py-2 px-4">10 USDT</td>
+            <td className="py-2 px-4">500 USDT</td>
+            <td className="py-2 px-4">متغییر بنا به شبکه تتر</td>
+          </tr>
+          <tr>
+            <td className="py-2 px-4 bg-indigo-300">شبکه ترون</td>
+            <td className="py-2 px-4">0.5 USDT</td>
+            <td className="py-2 px-4">0 USDT</td>
+            <td className="py-2 px-4">10 USDT</td>
+            <td className="py-2 px-4">؟</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
--- front-react/src/Pages/Commission/CommissionComponents/WidthdrawRate.js
+++ front-react/src/Pages/Commission/CommissionComponents/WidthdrawRate.js
@@ -3,41 +3,29 @@ import React, { useState, useEffect } from "react";
 export default function WidthdrawRate() {
   return (
     <div>
-      <div className=" m-8">
-        <div className="Row1  flex col-12">
-          <div
-            className="col2 col-6 py-2 -4xl"
-            style={{ backgroundColor: "#424874", color: "white" }}
-          >
-            کارمزد برداشت
-          </div>
-
-          <div
-            className="col2 col-6 py-2 -4xl"
-            style={{ backgroundColor: "#424874", color: "white" }}
-          >
-            میزان برداشت ریالی
-          </div>
-        </div>
-        <div className="Row1  flex col-12">
-          <div className="col2 col-6 py-2 -2xl" style={{ borderBottom: "90px" }}>
-            یک درصد میزان برداشت
-          </div>
-
-          <div className="col2 col-6 py-2 -2xl">کمتر از 400 هزار تومان</div>
-        </div>
-        <div className="Row2  flex col-12">
-          <div className="col2 col-6 py-2 -2xl">4000 تومان</div>
-
-          <div className="col2 col-6 py-2 -2xl">400 تا 50 میلیون تومان</div>
-        </div>
-        <div className="Row2  flex col-12">
-          <div className="col2 col-6 py-2 -2xl">
-            4000هزار تومان به ازای هر 50 میلیون تومان
-          </div>
-
-          <div className="col2 col-6 py-2 -2xl">بیشتر از 50 میلیون تومان</div>
-        </div>
+      <div className="m-8">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 bg-indigo-700 text-white">کارمزد برداشت</th>
+              <th className="py-2 px-4 bg-indigo-700 text-white">میزان برداشت ریالی</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="py-2 px-4 bg-indigo-300">یک درصد میزان برداشت</td>
+              <td className="py-2 px-4">کمتر از 400 هزار تومان</td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 bg-indigo-300">4000 تومان</td>
+              <td className="py-2 px-4">400 تا 50 میلیون تومان</td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 bg-indigo-300">4000هزار تومان به ازای هر 50 میلیون تومان</td>
+              <td className="py-2 px-4">بیشتر از 50 میلیون تومان</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
