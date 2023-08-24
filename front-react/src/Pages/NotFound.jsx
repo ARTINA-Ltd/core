6a67e2c169e1f6261b40dd8e400b69939bfb4dfc
jestemAria <radmehr.krash@gmail.com>
2023-08-24 11:44:31 +0330

    404page component now added to the project

---
+++ front-react/src/Pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestLayout from '../Layouts/TestLayout';
+
+const NotFound = () => {
+    return (
+        <TestLayout>
+            <div>
+                <h1>صفحه ی مورد نظر یافت نشد!</h1>
+                <p></p>
+            </div>
+        </TestLayout>
+    );
+};
+
+export default NotFound;
