b78a31aff24f5943f2d9f87161402d60429bfee6
zehi <you@example.com>
2024-01-28 16:18:51 +0330

    settings changes

--- ARTINA/settings.py
+++ ARTINA/settings.py
@@ -193,6 +193,13 @@ LOGGING = {
     },
 }
 
+# Celery Configuration
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'  # Adjust this based on your RabbitMQ setup
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
