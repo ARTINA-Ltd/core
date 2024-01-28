3f3b3cca81462888e6361badece0a5754663105f
zehi <you@example.com>
2024-01-28 16:11:00 +0330

    some change

--- test_rabbitmq_connection.py
+++ test_rabbitmq_connection.py
@@ -5,7 +5,7 @@ connection_params = pika.ConnectionParameters(
     host='127.0.0.1',
     port=5672,
     virtual_host='/',
-    credentials=pika.PlainCredentials(username='administrator', password='123qweasdZXC')
+    credentials=pika.PlainCredentials(username='root', password='123qweasdZXC')
 )
 
 # Establish connection
