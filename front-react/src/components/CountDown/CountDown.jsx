19e75bd38b2471b41db183ab4cb82ecd2b20f4bb
jestemAria <radmehr.krash@gmail.com>
2024-02-18 18:51:29 +0100

    countdown component added to the project

---
+++ front-react/src/components/CountDown/CountDown.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+
+const Countdown = ({ end_date }) => {
+  const endDate = new Date(end_date);
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+  // console.log("END DATE -------> ", endDate);
+
+  useEffect(() => {
+    if (end_date) {
+      const timer = setInterval(() => {
+        setTimeRemaining(calculateTimeRemaining())
+      }, 1000)
+      return () => clearInterval(timer)
+    }
+  }, [end_date])
+
+  function calculateTimeRemaining() {
+    const now = new Date();
+    const difference = endDate - now;
+
+    if (difference <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-2">
+        <p className="ml-2">
+          زمان باقی مانده تا پایان مزایده:
+        </p>
+        <p className="">
+          {timeRemaining.days}روز
+        </p>
+        <p className="">
+          {timeRemaining.hours}ساعت
+        </p>
+        <p className="">
+
+          {timeRemaining.minutes}دقیقه
+        </p>
+        <p className="">
+          {timeRemaining.seconds}ثانیه
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
