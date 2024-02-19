b32ec8d8fc4f1f46defd7606c809523c14d3c3c4
zehi <you@example.com>
2024-02-19 23:07:59 +0330

    add email verification to admin

--- Account/admin.py
+++ Account/admin.py
@@ -14,6 +14,7 @@ admin.site.register(models.UserBalance)
 admin.site.register(models.TransactionCurrency)
 admin.site.register(models.TransactionType)
 admin.site.register(models.PhoneVerification)
+admin.site.register(models.EmailVerification)
 admin.site.register(models.Wallet)
 admin.site.register(models.Transaction)
 
