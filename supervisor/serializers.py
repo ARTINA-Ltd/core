ac253c7a5067ccb1e5736a3495b63e15d0ccdc3e
zehi <you@example.com>
2024-03-14 23:11:44 +0330

    adding info of profile in supervisor

--- supervisor/serializers.py
+++ supervisor/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import DocumentApproval, SupervisorTicket
+from Account.models import Profile
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        
 
 class DocumentApprovalSerializer(serializers.ModelSerializer):
+    user_profile = ProfileSerializer()
     class Meta:
         model = DocumentApproval
         fields = '__all__'
