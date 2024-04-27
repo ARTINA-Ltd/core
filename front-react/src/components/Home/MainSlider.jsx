d7c490747c76d5c48a09a0923a7bd480df4734af
Mohammadhossein jahed <m.h.jahedi.7@gmail.com>
2024-04-27 23:10:06 +0330

    the top banner if fixed in build version as well

--- front-react/src/components/Home/MainSlider.jsx
+++ front-react/src/components/Home/MainSlider.jsx
@@ -1,26 +1,49 @@
-import React from "react";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-
+import React, { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { LazyLoadImage } from "./EmblaCarouselLazyLoadImage";
+import { NextButton, PrevButton, usePrevNextButtons } from "./EmblaCarouselArrowButtons";
+import "./MainSlider.css";
 const MainSlider = () => {
-  require("./MainSlider.css");
+  const options = { loop: true };
+  const [emblaRed, emblaApi] = useEmblaCarousel(options);
+  const [slidesInView, setSlidesInView] = useState([]);
 
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
+
+  const updateSlidesInView = useCallback((emblaApi) => {
+    setSlidesInView((slidesInView) => {
+      if (slidesInView.length === emblaApi.slideNodes().length) {
+        emblaApi.off("slidesInView", updateSlidesInView);
+      }
+      const inView = emblaApi.slidesInView().filter((index) => !slidesInView.includes(index));
+      return slidesInView.concat(inView);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    updateSlidesInView(emblaApi);
+    emblaApi.on("slidesInView", updateSlidesInView);
+    emblaApi.on("reInit", updateSlidesInView);
+  }, [emblaApi, updateSlidesInView]);
+  const slides = ["/2 - Copy.jpg", "/4.jpg"];
   return (
-    <>
-      <div className="w-full">
-        <Swiper navigation={true} loop={true} modules={[Navigation]} slidesPerView={1} spaceBetween={0}>
-          <SwiperSlide>
-            <img src="/4.jpg" className="object-cover w-full" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/2 - Copy.jpg" className="object-cover w-full" alt="" />
-          </SwiperSlide>
-        </Swiper>
+    <div className="embla" dir="ltr">
+      <div className="embla__viewport" ref={emblaRed}>
+        <div className="embla__container">
+          {slides.map((item, index) => (
+            <LazyLoadImage key={index} index={index} imgSrc={item} inView={slidesInView.indexOf(index) > -1} />
+          ))}
+        </div>
+        <div className="embla__controls">
+          <div className="embla__buttons">
+            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
