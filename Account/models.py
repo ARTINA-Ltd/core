d03f3080ffd7e41c652f50367157038824dc36f5
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-07-07 13:57:55 +0330

    Update models.py

--- Account/models.py
+++ Account/models.py
@@ -44,12 +44,13 @@ class Profile(models.Model):
                                                                           code='invalid_phone_number')])
     phone_number_verified = models.BooleanField(default=False)
     cell_number = models.CharField(max_length=11, verbose_name="شماره تلفن ثابت", null=True, blank=False)
+    
     shaba_number = models.CharField(max_length=24, verbose_name="shaba_number", null=True, blank=True)
     card_number = models.CharField(max_length=12, verbose_name="card_number", null=True, blank=True)    
     address = models.TextField(max_length=200, verbose_name="آدرس", null=True, blank=False)
     national_card_picture = models.TextField(verbose_name="عکس کارت ملی",null=True,blank=False,default="http://api.artina.org/static/images/Fig.png")
     profile_picture = models.TextField(verbose_name="عکس پروفایل",
-                                        null=True, blank=False, default="http://api.artina.org/static/images/default_C7876ge.webp",)
+                                        null=True, blank=False, default="",)
     email = models.EmailField(max_length=50, verbose_name="ایمیل", null=True, blank=False)
     email_verified = models.BooleanField(default=False)
     role = models.ForeignKey(Role, on_delete=models.CASCADE, default=1)
