6ed6f05bbdc1033bd04a0ccb35fe39d1fa5d5ee8
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-07-19 21:04:13 +0330

    Update views.py

--- exhibition/views.py
+++ exhibition/views.py
@@ -174,26 +174,24 @@ class ApplicationViewSet(viewsets.ModelViewSet):
     def update(self, request, pk=None):
         try:
             application = Application.objects.get(id=pk, exhibition__user=request.user)
-            id= application.exhibition.id
-            exhibition = Exhibition.objects.get(id=id)
+            exhibition= application.exhibition
         except Application.DoesNotExist:
             return Response({'error': 'Application not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         action = request.data.get('action', None)
         if action not in ['accept', 'ignored']:
             return Response({'error': 'Invalid action.'}, status=status.HTTP_400_BAD_REQUEST)
+        nfts = application.nft.all()
 
         if action == 'accept':
             application.status = 'accepted'
             for nft in nfts:
-                nft.in_exhibition = False
                 nft.is_for_sale = True
                 nft.start_date= exhibition.start_date
                 nft.end_date= exhibition.end_date                
                 nft.save()            
         elif action == 'ignored':
             application.status = 'ignored'
-            nfts = application.nft.all()
             for nft in nfts:
                 nft.in_exhibition = False              
                 nft.save()
