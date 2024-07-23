29d11c2c3a9b9f769b51831a30c8b6b826dce03b
Mohammadhossein jahed <m.h.jahedi.7@gmail.com>
2024-07-23 22:00:08 +0330

    Brand new Milesone Component 👌

--- front-react/src/components/Milestones/Milesone.jsx
+++ front-react/src/components/Milestones/Milesone.jsx
@@ -1,66 +1,67 @@
-import React, { useState } from "react";
-import ReactFlow, { Controls, Handle } from "reactflow";
-import "reactflow/dist/style.css";
-import CustomEdge from "./CustomEdge";
-import "./style.css";
+import { Timeline, TimelineEvent } from '@mailtop/horizontal-timeline'
+import { FaBug, FaRegCalendarCheck,FaQuestionCircle , FaRegFileAlt } from 'react-icons/fa'
+import { RiGuideFill } from "react-icons/ri";
+import { FcAdvertising } from "react-icons/fc";
+import { GiDeadWood } from "react-icons/gi";
+import { GrMoney } from "react-icons/gr";
+import { MdOutlineAttachMoney,MdSell  } from "react-icons/md";
 
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 300 }, data: { label: "Whats NFT?" }, type: "circleNode" },
-  { id: "2", position: { x: 100, y: 50 }, data: { label: "Create an NFT" }, type: "circleNode" },
-  { id: "3", position: { x: 220, y: 75 }, data: { label: "Artina's Nft Minting" }, type: "circleNode" },
-  { id: "4", position: { x: 400, y: 200 }, data: { label: "Monetization with NFT" }, type: "circleNode" },
-  { id: "5", position: { x: 500, y: 400 }, data: { label: "Advertise your NFT" }, type: "circleNode" },
-  { id: "6", position: { x: 600, y: 50 }, data: { label: "Sell NFT" }, type: "circleNode" },
-  { id: "7", position: { x: 700, y: 450 }, data: { label: "Gain Money" }, type: "circleNode" },
-];
 
-const initialEdges = [
-  { id: "e1-2", source: "1", target: "2", type: "customEdge" },
-  { id: "e2-3", source: "2", target: "3", type: "customEdge" },
-  { id: "e3-4", source: "3", target: "4", type: "customEdge" },
-  { id: "e4-5", source: "4", target: "5", type: "customEdge" },
-  { id: "e5-6", source: "5", target: "6", type: "customEdge" },
-  { id: "e6-7", source: "6", target: "7", type: "customEdge" },
-];
 
-const CircleNode = ({ id, data }) => (
-  <div
-    className="bg-primary text-primary-content rounded-full p-5 text-center relative box"
-    style={{
-      width: "50px",
-      height: "50px",
-      fontSize: "6px",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: "50%",
-    }}
-  >
-    {" "}
-    {data.label}
-    {id !== "1" && <Handle type="target" position="left" id="left" className="absolute left-0 top-1/2 transform -translate-y-1/2" style={{ width: "10px", height: "10px", background: "#000" }} />}
-    {id !== "7" && <Handle type="source" position="right" id="right" className="absolute right-0 top-1/2 transform -translate-y-1/2" style={{ width: "10px", height: "10px", background: "#000" }} />}
-  </div>
-);
-
-const nodeTypes = {
-  circleNode: CircleNode,
-};
-
-const edgeTypes = {
-  customEdge: CustomEdge,
-};
 
 const FlowComponent = () => {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+
 
   return (
-    <div className=" w-[80vw] mx-auto my-12" style={{ height: "80vh" }}>
-      <h1 className="mx-auto text-6xl text-center w-full">کاوش</h1>
-      <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes} edgeTypes={edgeTypes} zoomOnScroll={false} zoomOnPinch={false} zoomOnDoubleClick={false} fitView>
-        <Controls />
-      </ReactFlow>
+    <div className=" w-full mx-auto my-12 flex justify-center items-center " style={{ height: "80vh" }}>
+    <Timeline minEvents={5} placeholder>
+    <TimelineEvent
+      icon={MdOutlineAttachMoney}
+      title="Gain Money"
+      color="#38BDF8"
+      // subtitle='26/03/2019 09:51'
+    />
+    <TimelineEvent
+      icon={MdSell}
+      color="#4E45D0"
+      title='Sell NFT'
+      // subtitle='26/03/2019 09:51'
+    />
+    <TimelineEvent
+      icon={FcAdvertising}
+      title='Advertise Your NFT'
+      color="#F46FAC"
+      // subtitle='26/03/2019 09:51'
+    />
+    <TimelineEvent
+      icon={GrMoney}
+      title='Make Income'
+      color="#4E45D0"
+      // subtitle='Make Income'
+    />
+    <TimelineEvent
+      icon={FaRegCalendarCheck}
+      title="Artina's NFT Mint"
+      color="#FF6384  "
+      // subtitle='26/03/2019 09:51'
+    />
+    <TimelineEvent
+      color='#87a2c7'
+      icon={RiGuideFill}
+      title='Create NFT'
+      // subtitle='26/03/2019 09:51'
+    />
+    <TimelineEvent
+      color='#9c2919'
+      icon={FaQuestionCircle}
+      title="Whats's NFT?"
+      // subtitle='26/03/2019 09:51'
+      action={{
+        label: 'کلیک نکن',
+        onClick: () => window.alert('گفتم کلیک نکن چرا کلیک کردی')
+      }}
+    />
+  </Timeline>
     </div>
   );
 };
