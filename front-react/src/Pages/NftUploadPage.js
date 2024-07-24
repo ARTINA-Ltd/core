4874e3fd3fa917d4bf7b42d5f71027ac053b8465
Mohammadhossein jahed <m.h.jahedi.7@gmail.com>
2024-07-24 22:10:39 +0330

    issues fixed

--- front-react/src/Pages/NftUploadPage.js
+++ front-react/src/Pages/NftUploadPage.js
@@ -1,13 +1,23 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import UploadItem from "../components/UploadItem";
 import TestLayout from "../Layouts/TestLayout";
 import { useNavigate } from "react-router-dom";
+import { UserChangeContext, UserContext } from "../App.js";
+import { Notify } from "notiflix";
 
 function NFTUploadPage() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    localStorage.getItem("authTokens") === null && navigate("/login");
-  });
+
+  const [user,setUser] = useState(useContext(UserContext))
+  const userChange = useContext(UserChangeContext)
+  const navigate = useNavigate()
+useEffect(()=>{
+  if(!user){
+    Notify.warning("Please log in to you account")
+    navigate("/")
+  }
+ 
+
+},[user])
   return (
     <TestLayout connectWallet={true}>
       <UploadItem />
