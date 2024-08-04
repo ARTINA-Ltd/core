e41f7181ef9827241c99a0c1899156ba57096ae7
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-08-04 16:38:43 +0330

    Update tasks.py

--- core/tasks.py
+++ core/tasks.py
@@ -38,7 +38,7 @@ from datetime import timedelta
 from celery import shared_task
 from django.utils import timezone
 from .models import NFT
-from core.views import get_winner
+from .views import get_winner
 
 @shared_task
 
