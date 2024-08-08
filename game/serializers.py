5d0c5eaa153b264c3ab26d583a0d1568a30a936d
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-08-08 14:40:47 +0330

    Update serializers.py

--- game/serializers.py
+++ game/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Game, GameSession, UserGameProfile
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
