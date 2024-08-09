7cea96f51584aa8ca3df161e43bf40b6060e6498
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-08-09 23:01:34 +0330

    Update views.py

--- Account/views.py
+++ Account/views.py
@@ -1291,7 +1291,8 @@ class EmailMixin(viewsets.ViewSet):
             print(f"Verification code for {email}: {verification_code}")
         subject="verify email from ARTINA"
         message = f"your verfication code is : {verification_code}"
-        self.send_email(subject=subject,recipient_email=email, message=message)
+        recipient_email=email
+        self.send_email(subject,recipient_email, message)
         return Response({'success': 'email sent.'}, status.HTTP_200_OK)
 
 
