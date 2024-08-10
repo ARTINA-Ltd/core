4de766a03c0059b5924f2f68b85cf8825cbc1e20
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-08-10 13:08:09 +0330

    Update signals.py

--- Account/signals.py
+++ Account/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import UserBalance
+from .models import UserBalance,Profile, Role
+from supervisor.models import DocumentApproval
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -12,3 +13,17 @@ def create_user_profile(sender, instance, created, **kwargs):
             rial_available_balance=0
         )
 
+
+@receiver(post_save, sender=DocumentApproval)
+def update_user_role(sender, instance, **kwargs):
+    if instance.national_code_approved
+        try:
+            user_one_role = Role.objects.get(name="user_one")
+        except Role.DoesNotExist:
+            user_one_role = None
+
+        if user_one_role:
+            profile = instance.user_profile
+            profile.role = user_one_role
+            profile.save()
+            
