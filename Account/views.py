b22002f3c378f61c45d51d8ff89c6e061889f730
zehish <zehi.sh@gmail.com>
2024-09-03 14:45:47 +0330

    translate notif to persian

--- Account/views.py
+++ Account/views.py
@@ -211,7 +211,7 @@ class LoginViewSet(viewsets.ViewSet):
         current_time = timezone.now()
 
         # Send SMS with login details (implement your SMS sending logic here)
-        sms_message = f"Login successful on {current_time.strftime('%Y-%m-%d %H:%M:%S')}, IP: {ip_address}"
+        sms_message = f"ورود موفق شما در {current_time.strftime('%Y-%m-%d %H:%M:%S')}, IP: {ip_address}"
         # send_sms(user.phone_number, sms_message)  # Uncomment and implement this function
         NotifyUser.objects.create(user=user,text=sms_message)
         # Generate JWT tokens
