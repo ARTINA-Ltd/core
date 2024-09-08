aaa0d5c985f3e39e03e2326f4b85b3bcb64b2814
Mohammadhossein jahed <m.h.jahedi.7@gmail.com>
2024-09-08 23:38:35 +0330

    .

--- front-react/src/components/Cards/UserDashboardCards/ImageCard.jsx
+++ front-react/src/components/Cards/UserDashboardCards/ImageCard.jsx
@@ -9,21 +9,7 @@ import i18n from "./../../../i18n";
 import SimpleInput from "./../../Inputs/SimpleInput";
 import { Notify } from "notiflix";
 
-const ImageCard = ({
-  className,
-  children,
-  src,
-  price,
-  onClick,
-  tokenId,
-  showCancel,
-  showSell = false,
-  onClickShow,
-  onClickHide,
-  has_creator,
-  visible,
-  isForSale,
-}) => {
+const ImageCard = ({ className, children, src, price, onClick, tokenId, showCancel, showSell = false, onClickShow, onClickHide, has_creator, visible, isForSale }) => {
   const [isVisible, setIsVisible] = useState(visible);
   const [isExpanded, setIsExpanded] = useState(false);
   const [walletAddress, setWalletAddress] = useState("");
@@ -108,15 +94,8 @@ const ImageCard = ({
 
   return (
     <div className={className}>
-      <div
-        className="relative transition-all cursor-pointer h-3/4 mb-10"
-        onClick={onClick}
-      >
-        <img
-          src={src}
-          className="w-full rounded-lg object-cover p-2 h-full"
-          alt=""
-        />
+      <div className="relative transition-all cursor-pointer h-3/4 mb-10" onClick={onClick}>
+        <img src={src} className="w-full rounded-lg object-cover p-2 h-full" alt="" />
       </div>
       <div>
         {has_creator ? (
@@ -131,47 +110,15 @@ const ImageCard = ({
               </div>
               {showSell && (
                 <div className="flex items-center gap-2">
-                  <div
-                    className="transition-all py-2 rounded-lg px-2 hover:bg-neutral cursor-pointer duration-75 relative"
-                    onClick={isVisible ? handleClickHide : handleClickShow}
-                    onMouseOver={handleMouseOver}
-                    onMouseMove={handleMouseMove}
-                    onMouseOut={handleMouseOut}
-                  >
+                  <div className="transition-all py-2 rounded-lg px-2 hover:bg-neutral cursor-pointer duration-75 relative" onClick={isVisible ? handleClickHide : handleClickShow} onMouseOver={handleMouseOver} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
                     {isVisible ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.1"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.1" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                       </svg>
                     ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.1"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.1" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                       </svg>
                     )}
                     {showTooltip && (
@@ -202,47 +149,15 @@ const ImageCard = ({
               </div>
               {showSell ? (
                 <div className="flex items-end gap-2">
-                  <div
-                    className="transition-all py-2 rounded-lg px-2 hover:bg-neutral cursor-pointer duration-75"
-                    onClick={isVisible ? handleClickHide : handleClickShow}
-                    onMouseOver={handleMouseOver}
-                    onMouseMove={handleMouseMove}
-                    onMouseOut={handleMouseOut}
-                  >
+                  <div className="transition-all py-2 rounded-lg px-2 hover:bg-neutral cursor-pointer duration-75" onClick={isVisible ? handleClickHide : handleClickShow} onMouseOver={handleMouseOver} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
                     {isVisible ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.1"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.1" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                       </svg>
                     ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.1"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.1" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                       </svg>
                     )}
                     {showTooltip && (
@@ -259,11 +174,7 @@ const ImageCard = ({
                   </div>
                 </div>
               ) : (
-                showCancel && (
-                  <BorderButton onClick={handleCancel}>
-                    {t("cancelSell")}
-                  </BorderButton>
-                )
+                showCancel && <BorderButton onClick={handleCancel}>{t("cancelSell")}</BorderButton>
               )}
             </div>
             {
@@ -275,16 +186,16 @@ const ImageCard = ({
               <div class="collapse collapse-arrow overflow-visible">
                 <input type="checkbox" clas onClick={handleExpand} />
                 <div class="collapse-title text-xl sm:w-[75%]  font-medium m-0 ">
-                  <BorderButton className={"font-bold w-20 mx-auto"}>
-                    {isExpanded ? t("collapse") : t("sell")}
-                  </BorderButton>
+                  <BorderButton className={"font-bold w-20 mx-auto"}>{isExpanded ? t("collapse") : t("sell")}</BorderButton>
                 </div>
                 <div class="collapse-content w-full  overflow-visible ">
                   <SellArea tokenId={tokenId} cancel={handleExpand} />
                 </div>
               </div>
             )}
-            {dialog()}
+            {
+              //dialog()
+            }
           </Fragment>
         )}
       </div>
