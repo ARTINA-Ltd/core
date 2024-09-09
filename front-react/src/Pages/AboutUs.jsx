1011f81a6744b866d8d46f1ea5cadcf087d43557
Aria Radmehr <radmehr.krash@gmail.com>
2024-09-10 01:23:02 +0200

    minor updates

--- front-react/src/Pages/AboutUs.jsx
+++ front-react/src/Pages/AboutUs.jsx
@@ -9,13 +9,13 @@ const AboutUs = () => {
     <TestLayout>
       <div className="w-[55%] m-auto lg:w-4/5 md:w-11/12">
         <SimpleCard className={"bg-base-100 leading-[40px]"}>
-          <div className="text-[32px] mb-5">{t("aboutUs")}</div>
+          <div className="text-[32px] text-center mb-5">{t("aboutUs")}</div>
           <div className="text-[18px] mb-7 text-center sm:px-3 sm:text-[14px]">{t("fistParagraph")}</div>
-          <div className="text-[25px] mb-2 mr-5">{t("productOfArtina")}</div>
+          <div className="text-[25px] text-center mb-2 mr-5">{t("productOfArtina")}</div>
           <div className="text-[18px] mb-7 text-justify px-6 sm:px-3 sm:text-[14px]">{t("secondParagraph")}</div>
-          <div className="text-[25px] mb-2 mr-5">{t("goal")}</div>
+          <div className="text-[25px] text-center mb-2 mr-5">{t("goal")}</div>
           <div className="text-[18px] mb-7 text-justify px-6 sm:px-3 sm:text-[14px]">{t("thirdParagraph")}</div>
-          <div className="text-[25px] mb-2 mr-5">{t("foundation")}</div>
+          <div className="text-[25px] text-center mb-2 mr-5">{t("foundation")}</div>
           <div className="text-[18px] text-justify px-6 sm:px-3 sm:text-[14px]">{t("fourthParagraph")}</div>
         </SimpleCard>
       </div>
