1fd7e85a0e3d25c57a934d88f92252b97d995067
zehish <zehi.sh@gmail.com>
2024-09-15 23:33:26 +0330

    fixing urls

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GameViewSet, UserProfileViewSet, LeaderboardViewSet
+from .views import *
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
