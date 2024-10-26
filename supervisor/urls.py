c5e8188986487f646f17bf8013355ff525af079b
zehish <zehi.sh@gmail.com>
2024-10-26 11:09:13 +0330

    deleting unused urls

--- supervisor/urls.py
+++ supervisor/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DocumentApprovalViewSet, SupervisorTicketViewSet,RejectionMessageViewSet
+from .views import DocumentApprovalViewSet, SupervisorTicketViewSet
 
 router = DefaultRouter()
 router.register(r'document-approvals', DocumentApprovalViewSet, basename='document-approval')
 router.register(r'supervisor-tickets', SupervisorTicketViewSet, basename='supervisor-ticket')
-router.register(r'rejection-messages', RejectionMessageViewSet, basename='Rejection-Message')
 
 urlpatterns = [
     path('', include(router.urls)),
