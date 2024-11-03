82df29f94c92abecfb3f8dd46e202eae80bb00be
zehish <zehi.sh@gmail.com>
2024-11-03 19:00:15 +0330

    fixing settings

--- ARTINA/urls.py
+++ ARTINA/urls.py
@@ -40,4 +40,6 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
