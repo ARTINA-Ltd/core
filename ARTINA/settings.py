bc4426da18d94362fbef13083e1b6cbdf011aeb7
zahra shah morad zade <40887905+zehish@users.noreply.github.com>
2024-11-04 23:37:25 +0330

    Update settings.py

--- ARTINA/settings.py
+++ ARTINA/settings.py
@@ -265,3 +265,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
